cac4e47100c7f295a9addbcbf74664990a9f369c
Tianrui Wei <tianrui-wei@outlook.com>
2020-08-13 21:22:58 +0800

    corrected early uart print, corrected interrupt, added more flags

--- arch/mips/include/asm/mach-phoenix/cpu-feature-overrides.h
+++ arch/mips/include/asm/mach-phoenix/cpu-feature-overrides.h
@@ -1,16 +1,22 @@
-#ifndef __ASM_MACH_PHOENIX_CPU_FEATURE_OVERIDES_
-#define __ASM_MACH_PHOENIX_CPU_FEATURE_OVERIDES_
+#ifndef __ASM_MACH_PHOENIX_CPU_FEATURE_OVERIDES_H
+#define __ASM_MACH_PHOENIX_CPU_FEATURE_OVERIDES_H
 
+#include <asm/cpu.h>
+
+#define cpu_has_prefetch 0
+#define cpu_has_dc_aliases 0
 #define cpu_has_watch 0
 #define cpu_has_divec 0
 #define cpu_has_mcheck 0
 #define cpu_has_ejtag 0
-#define cpu_has_prefetch 0
-#define cpu_has_32fpr 0
-#define cpu_has_vce 0
-#define cpu_has_mcheck 0
 #define cpu_has_llsc 0
-#define cpu_has_mips32r1 1
+#define cpu_has_dsp 0
+#define cpu_has_dsp2 0
+#define cpu_has_mips16 0
+#define cpu_has_mips16e2 0
+#define cpu_has_mips3d 0
+#define cpu_has_mipsmt 0
+#define cpu_has_smartmips 0
 
 
 #endif
--- arch/mips/phoenix/prom.c
+++ arch/mips/phoenix/prom.c
@@ -1,8 +1,9 @@
 #include <linux/of_fdt.h>
 #include <asm/prom.h>
 
+// from non-trivial-mips
 #if IS_ENABLED(CONFIG_PHOENIX_ARTIX7)
-#define EARLY_PRINTK_UART_BASE 0xbfe40000
+#define EARLY_PRINTK_UART_BASE 0xbfd00000
 #define PHOENIX_UART_OUTB(addr, byte) writeb((byte), (uint32_t *)(EARLY_PRINTK_UART_BASE + addr))
 #endif
 
@@ -19,21 +20,21 @@ void __init prom_init(void)
 #if IS_ENABLED(CONFIG_PHOENIX_ARTIX7)
     /** turn on the uart fifo register, reset all values,
      * set the interrupt threshold to 8 byte */
-    PHOENIX_UART_OUTB(0x08, 0x83);
+    //PHOENIX_UART_OUTB(0x08, 0x81);
     /** set uar lcr register DLABbit to 1
      * this will use 0x0, 0x1 as divisor latch */
-    PHOENIX_UART_OUTB(0x0c, 0x80);
+    //PHOENIX_UART_OUTB(0x0c, 0x80);
     /** set the baud rate to 57600 */
-    PHOENIX_UART_OUTB(0x04, 0x23);
-    PHOENIX_UART_OUTB(0x00, 0x08);
+    //PHOENIX_UART_OUTB(0x04, 0x00);
+    //PHOENIX_UART_OUTB(0x00, 0x36);
 
     // 8 data bits, 1 stop bit, parity off; turn off DLAB latch
-    PHOENIX_UART_OUTB(0x0C, 0x03);
+    //PHOENIX_UART_OUTB(0x0C, 0x03);
 
     // turn on rts and dts model control
-    PHOENIX_UART_OUTB(0x10, 0x3);
+    //PHOENIX_UART_OUTB(0x10, 0x0);
     // Enable rcv interrupts
-    PHOENIX_UART_OUTB(0x04, 0x01);
+    //PHOENIX_UART_OUTB(0x04, 0x01);
 #endif
 }
 
